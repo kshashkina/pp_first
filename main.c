6b67bdefbe18d753446b48bde8d85fc5170c952b
Katherine Shashkina <kshashkina@kse.org.ua>
2023-09-06 11:16:52 +0300

    first milestone(switch structure, started developing commands 1-3)

--- main.c
+++ main.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!");
+    int commandNumber;
+    char newWord [100];
+    char fileName [50];
+    int i = 0;
+
+    while (1){
+        printf("Write your command:\n");
+        scanf("%d", &commandNumber);
+        switch (commandNumber) {
+            case 1:
+                printf("Enter text to append:\n");
+                scanf("%s", newWord);
+                printf("Your word have been saved!\n");
+                break;
+            case 2:
+                while (newWord[i] != 0){
+                    i++;
+                }
+                newWord[i] = '\r';
+                printf("New line has been added!\n");
+                break;
+            case 3:
+                printf("Enter the file name for saving:\n");
+                scanf("%s", fileName);
+                printf("Name has been saved! \n");
+            case 4:
+                break;
+            case 5:
+                printf("%s", newWord);
+            case 6:
+                break;
+            case 7:
+                break;
+            default:
+                break;
+        }
+    }
     return 0;
 }
