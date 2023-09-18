30c0cb7d8da8c5f3c25172c1e059449b644ad402
Katherine Shashkina <kshashkina@kse.org.ua>
2023-09-18 09:14:47 +0300

    implemented linked list for string manipulation and changed data saving logic

--- main.c
+++ main.c
@@ -1,35 +1,98 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+// Define a structure for a node in the linked list
+typedef struct Node {
+    char data;
+    struct Node* next;
+} Node;
+
+// Function to create a new node
+Node* createNode(char data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert a node at the end of the linked list
+void insertNodeEnd(Node** head, char data) {
+    Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        Node* current = *head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+}
+
+// Function to insert a node at a specific position in the linked list
+void insertNodeAtPosition(Node** head, char data, int position) {
+    Node* newNode = createNode(data);
+
+    if (position == 0) {
+        newNode->next = *head;
+        *head = newNode;
+    } else {
+        Node* current = *head;
+        int i;
+        for (i = 0; i < position - 1 && current != NULL; i++) {
+            current = current->next;
+        }
+        if (current != NULL) {
+            newNode->next = current->next;
+            current->next = newNode;
+        } else {
+            printf("Invalid position.\n");
+            free(newNode);
+        }
+    }
+}
+
+// Function to display the linked list
+void displayList(Node* head) {
+    Node* current = head;
+    while (current != NULL) {
+        printf("%c", current->data);
+        current = current->next;
+    }
+    printf("\n");
+}
+
+// Function to free the linked list
+void freeList(Node* head) {
+    Node* current = head;
+    while (current != NULL) {
+        Node* temp = current;
+        current = current->next;
+        free(temp);
+    }
+}
 
 int main() {
     int commandNumber;
-    char newWord [100]  = {'\0'}; ;
-    char extraWord [100];
-    char fileName [50];
-    int i = 0;
+    Node* newWord = NULL; // Initialize the linked list
+    char inputLine[100];
+    char fileName[50];
 
-    while (1){
+    while (1) {
         printf("Write your command:\n");
         scanf("%d", &commandNumber);
         switch (commandNumber) {
             case 1:
                 printf("Enter text to append:\n");
-                if (newWord[0]==0){
-                    scanf("%s", newWord);
-                    printf("Your word have been saved!\n");
+                scanf(" %[^\n]s", inputLine); // Read a line of text
+                for (int i = 0; i < strlen(inputLine); i++) {
+                    insertNodeEnd(&newWord, inputLine[i]);
                 }
-                else{
-                    scanf("%s", extraWord);
-                    strcat(newWord,extraWord);
-                    printf("Your word have been saved!\n");
-                }
-
+                printf("Your word has been saved!\n");
                 break;
             case 2:
-                while (newWord[i] != 0){
-                    i++;
-                }
-                newWord[i] = '\n';
+                insertNodeEnd(&newWord, '\n');
                 printf("New line has been added!\n");
                 break;
             case 3:
@@ -39,7 +102,7 @@ int main() {
             case 4:
                 break;
             case 5:
-                printf("%s \n", newWord);
+                displayList(newWord);
                 break;
             case 6:
                 break;
